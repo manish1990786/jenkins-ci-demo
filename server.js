1565d40f7ef5c2acdd4af462550236ea2f0e385c
manish agrawal <manishbansal019@gmail.com>
2025-02-26 22:42:11 +0530

    Ädded new feature

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is  running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
